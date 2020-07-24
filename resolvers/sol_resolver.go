7f0baf067fc304430b1791622d9a2c1c626e000c
Anže Stražar <strazar@gmail.com>
2020-07-24 21:48:51 +0200

    Remove debug print

--- resolvers/sol_resolver.go
+++ resolvers/sol_resolver.go
@@ -1,16 +1,14 @@
 package resolvers
 
 import (
-	"fmt"
 	"weather-on-mars-interface/types"
 )
 
 type SolResolver struct {
-	sol *types.Sol
+	sol types.Sol
 }
 
 func (a *SolResolver) At() *AtResolver {
-	fmt.Println("TEST:::::")
 	atResult := a.sol.Data.At
 	return &AtResolver{&atResult}
 }
