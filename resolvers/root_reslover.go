159ab2b3526e59741e7830dc85b0735813d68e2e
Anže Stražar <strazar@gmail.com>
2020-07-24 21:49:23 +0200

    Add RLock & RUnlock

--- resolvers/root_reslover.go
+++ resolvers/root_reslover.go
@@ -12,18 +12,23 @@ func NewRootResolver(cache *cache.Cache) *RootResolver {
 	return &RootResolver{cache: cache}
 }
 
-func (r *RootResolver) Sol(args struct{ ID *string }) *[]*SolResolver {
+func (r *RootResolver) Sol(args struct{ ID string }) *SolResolver {
+	r.cache.SolMutex.RLock()
+	defer r.cache.SolMutex.RUnlock()
+	solResult := r.cache.SolCache[args.ID]
+
+	return &SolResolver{solResult}
+}
+
+func (r *RootResolver) Sols() *[]*SolResolver {
 	resolversResults := make([]*SolResolver, 0)
 
-	if args.ID == nil {
-		for _, solResult := range r.cache.SolCache {
-			resolversResults = append(resolversResults, &SolResolver{&solResult})
-		}
-
-		return &resolversResults
+	r.cache.SolMutex.RLock()
+	for _, solResult := range r.cache.SolCache {
+		resolversResults = append(resolversResults, &SolResolver{solResult})
 	}
-	solResult := r.cache.SolCache[*args.ID]
 
-	resolversResults = append(resolversResults, &SolResolver{&solResult})
+	defer r.cache.SolMutex.RUnlock()
+
 	return &resolversResults
 }
